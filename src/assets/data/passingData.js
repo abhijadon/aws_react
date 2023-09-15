51ac730ac59ffb998d7762c3d1a405d59245686c
abhijadon <134587271+abhijadon@users.noreply.github.com>
2023-09-15 17:53:54 +0530

    this

---
+++ src/assets/data/passingData.js
@@ -0,0 +1,47 @@
+const passinsYear = [
+  {
+    name: "2023",
+    degrees: [
+      {
+        name: "migrate",
+        prices: ["1000"],
+      },
+      {
+        name: "degree",
+        prices: [1000],
+      },
+      {
+        name: "migrate",
+        prices: [1000],
+      },
+    ],
+  },
+  {
+    name: "2022",
+    degrees: [
+      {
+        name: ["Migrate", "degree", "Pro migrate"],
+        prices: [1000],
+      },
+    ],
+  },
+  {
+    name: "2021",
+    degrees: [
+      {
+        name: ["Migrate", "degree", "Pro migrate"],
+        prices: [1000],
+      },
+    ],
+  },
+  {
+    name: "2020",
+    degrees: [
+      {
+        name: "",
+      },
+    ],
+  },
+];
+
+export default passinsYear;
